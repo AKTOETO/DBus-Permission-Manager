07e85e2cd7b84c31cf9dfb4e8d99c70f085d95cc
AKTOETO <bogdan.plockiy@mail.ru>
2024-10-01 02:28:57 +0300

    Unnecessary targets have been removed from cmake. Bug fixing: There was no check for the existence of services in main_app

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -15,31 +15,46 @@ int main() {
 
   // пока время не получено, пытаемся его получить
   while (!got_time) {
-    got_time = 1;
     try {
       // получение времени
       time = time_client.getSystemTime();
+      got_time = 1;
 
     } catch (const sdbus::Error &e) {
+
+      got_time = 0;
+
       // если произошла ошибка sdbus, обрабатываем ее
       if (e.getName() == "com.system.time.Error.UnauthorizedAccess") {
-        got_time = 0;
         // если же ошибка связана с неполученным доступом, получаем его
         std::cout << "Доступа к SystemTime нет. Запрашиваем его.\n";
 
+        // Подключение к сервису с правами доступа
         PermissionsClient perm_client(
             std::move(Utils::createProxy("com.system.permissions")));
+
+        // запрашивание прав доступа
         perm_client.requestPermission(Permissions::PermissionType::SystemTime);
+      } else {
+
+        // произошло другое DBus исключение
+        std::cout << "Ошибка: " << e.getName() << " : " << e.getMessage()
+                  << std::endl;
+        // если какой-то из сервисов не работает, выходим
+        if (e.getName() == "org.freedesktop.DBus.Error.ServiceUnknown")
+          break;
       }
 
     } catch (const std::exception &e) {
+
       // если произошла другая ошибка, печатаем ее
       std::cerr << "Ошибка: " << e.what() << std::endl;
     }
   }
 
   // печать времени
-  Utils::printTime(std::cout, time);
+  if (got_time)
+    Utils::printTime(std::cout, time);
 
   return 0;
 }
